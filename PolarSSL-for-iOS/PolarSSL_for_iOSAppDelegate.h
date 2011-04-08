c6cc51633a144e6077da08e27d958a9e38e80a45
Felix Schulze <code@felixschulze.de>
2011-04-08 09:03:57 +0200

    Initial Commit

---
+++ PolarSSL-for-iOS/PolarSSL_for_iOSAppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  PolarSSL_for_iOSAppDelegate.h
+//  PolarSSL-for-iOS
+//
+//  Created by x2on on 08.04.11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PolarSSL_for_iOSAppDelegate : NSObject <UIApplicationDelegate> {
+
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
---
+++ PolarSSL-for-iOS/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  PolarSSL-for-iOS
+//
+//  Created by x2on on 08.04.11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[])
+{
+    NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, nil);
+    [pool release];
+    return retVal;
+}
